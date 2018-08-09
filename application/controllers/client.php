e1a90943972c59845fa5170f0aca376a74434153
Ahmad Syafiq Kamil <ahmadsyafiqkamil@gmail.com>
2018-08-09 15:59:19 +0700

    entah

--- application/controllers/client.php
+++ application/controllers/client.php
@@ -7,29 +7,14 @@ class Client extends CI_Controller
   public function __construct()
   {
     parent::__construct();
-    // $this->template->set_error_view("error/login_error.php");
-    // $this->template->loadResponsiveSidebar("layout/sidebar");
-    // $this->template->set_layout("layout/client_layout.php");
+    $this->template->set_error_view("error/login_error.php");
+    $this->template->loadResponsiveSidebar("layout/sidebar");
+    $this->template->set_layout("layout/client_layout.php");
 
   }
   public function index()
   {
-    // $this->template->loadExternal(
-    //   '	<script type="text/javascript" src="assets/js/plugins/ui/moment/moment.min.js"></script>
-    //   <script type="text/javascript" src="assets/js/plugins/ui/fullcalendar/fullcalendar.min.js"></script>
-    //
-    //   <script type="text/javascript" src="assets/js/core/app.js"></script>
-    //   <script type="text/javascript" src="assets/js/pages/extra_fullcalendar.js"></script>
-    //
-    //   <script type="text/javascript" src="assets/js/plugins/ui/ripple.min.js"></script>'
-    // );
-    // $this->template->loadContent("client/index.php", array());
-    $this->load->view('layout/client_layout');
-  }
-  public function instansi()
-  {
-
-    $this->template->loadContent("client/instansi.php", array());
+      $this->template->loadContent("client/index.php", array());
   }
 }
 
